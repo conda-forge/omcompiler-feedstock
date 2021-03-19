5a1dd29fc79cdce326532ad1108be06b6409f621
Julien Schueller <schueller@phimeca.com>
2021-03-19 15:59:02 +0100

    Update to 1.16.5

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,7 @@
 
 git submodule -q sync
 git submodule -q update --init --recursive
+cd OMCompiler
 
 # dont include the build prefix
 export CC=`basename ${CC}`
@@ -13,15 +14,12 @@ export CXXFLAGS="${CXXFLAGS} -std=c++14"
 # error: expected '=', ',', ';', 'asm' or '__attribute__' before 'void'
 cd 3rdParty/FMIL && curl -L https://raw.githubusercontent.com/conda-forge/fmilib-feedstock/master/recipe/undef_gnu_source.patch | patch -p1 && cd -
 
-# help sundials find lapack
-sed -i "s|DLAPACK_ENABLE:Bool=ON|DLAPACK_ENABLE:Bool=ON -DCMAKE_PREFIX_PATH=${PREFIX}|g" Makefile.common
-
 # help find conda dependencies in prefix
 sed -i "s|\-lOpenModelicaCompiler|\-L${PREFIX}/lib \-lOpenModelicaCompiler|g" common/m4/omhome.m4
 sed -i "s|RT_LDFLAGS_SHARED=\"\-Wl,\-rpath\-link,|RT_LDFLAGS_SHARED=\"\-Wl,\-rpath\-link,${PREFIX}/lib \-Wl,\-rpath\-link,|g" configure.ac
 
 # add fpic to fmi build flags
-sed -i "s|\-DOMC_MINIMAL_RUNTIME=1 \-DCMINPACK_NO_DLL=1|\-DOMC_MINIMAL_RUNTIME=1 \-DCMINPACK_NO_DLL=1 \-fPIC|g" SimulationRuntime/fmi/export/buildproject/configure.ac Compiler/Script/CevalScriptBackend.mo
+sed -i "s|\-DCMINPACK_NO_DLL=1|\-DCMINPACK_NO_DLL=1 \-fPIC|g" SimulationRuntime/fmi/export/buildproject/configure.ac Compiler/Script/CevalScriptBackend.mo
 
 # Compiler/runtime/libomcruntime-boot.so: undefined reference to `libiconv
 sed -i "s|\-lzmq|\-lzmq \-liconv|g" configure.ac
