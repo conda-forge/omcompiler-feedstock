bc0a4f10bc72c1d2302653756b1cea8f726766e3
Julien Schueller <schueller@phimeca.com>
2022-10-02 09:53:42 +0200

    Update to 1.19.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,23 +10,23 @@ export CC=`basename ${CC}`
 export CPP=`basename ${CPP}`
 
 # netstream does not build with conda-forge's default c++1z
-export CXXFLAGS="${CXXFLAGS} -std=c++14"
+#export CXXFLAGS="${CXXFLAGS} -std=c++14"
 
 # error: expected '=', ',', ';', 'asm' or '__attribute__' before 'void'
-curl -L https://github.com/OpenModelica/OMCompiler-3rdParty/pull/89.patch | patch -p1 -d 3rdParty
+#curl -L https://github.com/OpenModelica/OMCompiler-3rdParty/pull/89.patch | patch -p1 -d 3rdParty
 
 # help find conda dependencies in prefix
-sed -i "s|\-lOpenModelicaCompiler|\-L${PREFIX}/lib \-lOpenModelicaCompiler|g" common/m4/omhome.m4
-sed -i "s|RT_LDFLAGS_SHARED=\"\-Wl,\-rpath\-link,|RT_LDFLAGS_SHARED=\"\-Wl,\-rpath\-link,${PREFIX}/lib \-Wl,\-rpath\-link,|g" configure.ac
+#sed -i "s|\-lOpenModelicaCompiler|\-L${PREFIX}/lib \-lOpenModelicaCompiler|g" common/m4/omhome.m4
+#sed -i "s|RT_LDFLAGS_SHARED=\"\-Wl,\-rpath\-link,|RT_LDFLAGS_SHARED=\"\-Wl,\-rpath\-link,${PREFIX}/lib \-Wl,\-rpath\-link,|g" configure.ac
 
 # add fpic to fmi build flags
-sed -i "s|\-DCMINPACK_NO_DLL=1|\-DCMINPACK_NO_DLL=1 \-fPIC|g" SimulationRuntime/fmi/export/buildproject/configure.ac Compiler/Script/CevalScriptBackend.mo
+#sed -i "s|\-DCMINPACK_NO_DLL=1|\-DCMINPACK_NO_DLL=1 \-fPIC|g" SimulationRuntime/fmi/export/buildproject/configure.ac Compiler/Script/CevalScriptBackend.mo
 
 # Compiler/runtime/libomcruntime-boot.so: undefined reference to `libiconv
-sed -i "s|\-lzmq|\-lzmq \-liconv|g" configure.ac
+#sed -i "s|\-lzmq|\-lzmq \-liconv|g" configure.ac
 
 # KLU detection error
-sed -i "s|try_compile(LTEST_OK \${KLUTest_DIR} \${KLUTest_DIR} ltest OUTPUT_VARIABLE MY_OUTPUT)|set(LTEST_OK TRUE)|g" 3rdParty/sundials-5.4.0/config/SundialsKLU.cmake
+#sed -i "s|try_compile(LTEST_OK \${KLUTest_DIR} \${KLUTest_DIR} ltest OUTPUT_VARIABLE MY_OUTPUT)|set(LTEST_OK TRUE)|g" 3rdParty/sundials-5.4.0/config/SundialsKLU.cmake
 
 autoconf
 ./configure --prefix=${PREFIX}
