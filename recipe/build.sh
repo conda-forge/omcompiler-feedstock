4af8c0e072ebd9f8bd41909d22ce83ae557bd779
Julien Schueller <schueller@phimeca.com>
2025-04-25 10:13:02 +0200

    PR13782

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ git submodule -q update --init --recursive
 curl -L https://github.com/OpenModelica/OMCompiler-3rdParty/pull/89.patch | patch -p1 -d OMCompiler/3rdParty
 
 # OMCompiler/Compiler/runtime/HpcOmBenchmarkExt.cpp:24:10: fatal error: expat.h: No such file or directory
-sed -i "156itarget_link_libraries(omcbackendruntime PUBLIC omc::3rd::FMIL::expat)" OMCompiler/Compiler/runtime/CMakeLists.txt
+curl -L https://github.com/OpenModelica/OpenModelica/pull/13782.patch | patch -p1
 
 cmake ${CMAKE_ARGS} -G "Ninja" -LAH \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
