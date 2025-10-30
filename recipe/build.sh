f484b5ac4a87051a88d556e04d9a7965f21c8c7c
Julien Schueller <schueller@phimeca.com>
2025-10-30 20:21:02 +0100

    expat.h

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,9 +6,6 @@ git submodule -q update --init --recursive
 # error: expected '=', ',', ';', 'asm' or '__attribute__' before 'void'
 curl -L https://github.com/OpenModelica/OMCompiler-3rdParty/pull/89.patch | patch -p1 -d OMCompiler/3rdParty
 
-# OMCompiler/Compiler/runtime/HpcOmBenchmarkExt.cpp:24:10: fatal error: expat.h: No such file or directory
-curl -L https://github.com/OpenModelica/OpenModelica/pull/13782.patch | patch -p1
-
 cmake ${CMAKE_ARGS} -G "Ninja" -LAH \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
