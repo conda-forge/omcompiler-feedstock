be5b79f79f66b3019e06367b4d12583740c3b3de
Julien Schueller <schueller@phimeca.com>
2026-03-28 08:29:31 +0100

    sub

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,25 @@
 
 set -x
 
-# git config -f .gitmodules submodule.OMCompiler/3rdParty.url https://github.com/OpenModelica/OMCompiler-3rdParty.git
-# git submodule -q sync --recursive OMCompiler/3rdParty
-# git submodule -q update --force --init --recursive OMCompiler/3rdParty
+git config -f .gitmodules submodule.OMCompiler/3rdParty.url https://github.com/OpenModelica/OMCompiler-3rdParty.git
+git submodule -q sync --recursive OMCompiler/3rdParty
+git submodule -q update --force --init --recursive OMCompiler/3rdParty
+
+git config -f .gitmodules submodule.OMOptim.url https://github.com/OpenModelica/OMOptim.git
+git submodule -q sync --recursive OMOptim
+git submodule -q update --force --init --recursive OMOptim
+
+git config -f .gitmodules submodule.OMSimulator.url https://github.com/OpenModelica/OMSimulator.git
+git submodule -q sync --recursive OMSimulator
+git submodule -q update --force --init --recursive OMSimulator
+
+git config -f .gitmodules submodule.OMSens_Qt.url https://github.com/OpenModelica/OMSens_Qt.git
+git submodule -q sync --recursive OMSens_Qt
+git submodule -q update --force --init --recursive OMSens_Qt
+
+git config -f .gitmodules submodule.OMCompiler/Compiler/boot/bomc.url https://github.com/OpenModelica/OMBootstrapping.git
+git submodule -q sync --recursive OMCompiler/Compiler/boot/bomc
+git submodule -q update --force --init --recursive OMCompiler/Compiler/boot/bomc
 
 # error: expected '=', ',', ';', 'asm' or '__attribute__' before 'void'
 curl -L https://github.com/OpenModelica/OMCompiler-3rdParty/pull/89.patch | patch -p1 -d OMCompiler/3rdParty
